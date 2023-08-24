023aaf5db13039514c3674dfdc13ac6047e6ee5c
Anson Savage <ansonsavage@gmail.com>
2023-08-23 18:37:42 -0600

    Created a little function that constructs a fake maya file path from the houdini file

--- pipe/accomplice/software/maya/pipe/shelves/open_shot.py
+++ pipe/accomplice/software/maya/pipe/shelves/open_shot.py
@@ -4,6 +4,7 @@ import pipe
 from PySide2 import QtWidgets, QtGui, QtCore
 
 class ShotFileDialog(QtWidgets.QDialog):
+    """ A PyQt class for holding the list of shot files """
     def __init__(self, shot_names, parent=None):
         super(ShotFileDialog, self).__init__(parent)
         self.setWindowTitle("Open Shot File")
@@ -27,12 +28,27 @@ class ShotFileDialog(QtWidgets.QDialog):
         
         self.setLayout(layout)
 
+def open_file(file_path):
+    """ Opens a new Maya file """
+    cmds.file(file_path, open=True, force=True)
+
+def create_new_file(file_path):
+    """ Creates a new Maya file """
+    cmds.file(new=True, force=True)
+    cmds.file(rename=file_path)
+    cmds.file(save=True)
+    
+def create_maya_file_path_from_houdini_shot_file(houdini_file_path: str):
+    """ A really hacky thing that needs to be deleted that creates what might be the path to the maya version of this file """
+    assert houdini_file_path.endswith(".hipnc")
+    return houdini_file_path.replace(".hipnc", ".mb") # Switch out the file extension :)
+
 def open_shot_file():
     unsaved_changes = cmds.file(query=True, modified=True)
     if unsaved_changes:
         response = cmds.confirmDialog(
             title="Unsaved Changes",
-            message="The current file has unsaved changes. Continue anyways?",
+            message="The current file has unsaved changes. Continue anyway?",
             button=["Continue", "Cancel"],
             defaultButton="Cancel",
             cancelButton="Cancel",
@@ -45,23 +61,21 @@ def open_shot_file():
     
     shot_dialog = ShotFileDialog(shot_names)
     result = shot_dialog.exec_()
-    print("Result: ", result)
     
     if result == QtWidgets.QDialog.Accepted:
         selected_item = shot_dialog.shot_list.currentItem()
-        print("Selected Item: ", selected_item.text())
         if selected_item:
             shot_name = selected_item.text()
             shot = pipe.server.get_shot(shot_name)
             
-            file_path = shot.get_shotfile()
+            file_path = shot.get_shotfile(type='anim')
+            file_path = create_maya_file_path_from_houdini_shot_file(file_path) # TODO: This is scary, fix this! :)
+            print(file_path)
             
-            if os.path.isfile(file_path):
-                cmds.file(file_path, open=True, force=True)
-            else:
-                cmds.file(new=True, force=True)
-                cmds.file(rename=file_path)
-                cmds.file(save=True)
-                cmds.file(save=True)
+            if os.path.isfile(file_path): # If the file exists
+                # Open the file
+                open_file(file_path)
+            else: # Otherwise, open a new tile and save it
+                create_new_file(file_path)
 
 open_shot_file()
