f332515d5236000ed9468d987a489aef6bf7d89c
Anson Savage <ansonsavage@gmail.com>
2023-09-18 10:50:01 -0600

    Began object orienting some of the SGDatabase file...

--- pipe/database/ShotGridDatabase.py
+++ pipe/database/ShotGridDatabase.py
@@ -1,6 +1,7 @@
 import os
 import logging as log
 from typing import Iterable, Set, Sequence, Union
+from abc import ABC, abstractmethod
 
 from .baseclass import Database
 from shared.object import Asset
@@ -14,83 +15,28 @@ else:
 
 import shotgun_api3 # Here's some good API reference: https://developer.shotgridsoftware.com/python-api/reference.html
 
-
 class ShotGridDatabase(Database):
-    
-    _untracked_asset_types = [ 
-                        'Character', 
-                        'FX', 
-                        'Graphic', 
-                        'Matte Painting', 
-                        'Tool', 
-                        'Font'
-                    ]
-    
     def __init__(self,
                  shotgun_server: str,
                  shotgun_script: str,
                  shotgun_key:    str,
                  project_id:     int,
                 ):
-        self.sg= shotgun_api3.Shotgun(shotgun_server, shotgun_script, shotgun_key)
+        sg= shotgun_api3.Shotgun(shotgun_server, shotgun_script, shotgun_key)
         self.PROJECT_ID = project_id
+        self.shot_grid_query_helper = ShotGridQueryHelper(self, sg)
         super().__init__()
         pass
-    
+
     def get_asset(self, name: str) -> Asset:
-        filters = [
-            [ 'project', 'is', { 'type': 'Project', 'id': self.PROJECT_ID } ],
-            [ 'sg_status_list', 'is_not', 'oop' ],
-            [ 'sg_path', 'ends_with', f'/{name}' ],
-            { 
-                'filter_operator': 'all',
-                'filters': [ 
-                    [ 'sg_asset_type', 'is_not', t ] for t in self._untracked_asset_types
-                ], 
-            },
-        ]
-
-        fields = [
-            'code',
-            'sg_path',
-            'id'
-        ]
-
-        asset = self.sg.find_one('Asset', filters, fields)
+        asset = self.shot_grid_query_helper.get_one_asset_by_name(name)
         sg_path = asset['sg_path']
         name = os.path.basename(sg_path)
-        return Asset(name, path = asset['sg_path'], id = asset['id'])
+        return Asset(name, path = asset['sg_path'])
         
     def get_assets(self, names: Iterable[str]) -> Set[Asset]:
-        # TODO: Ideally it'd be nice to not grab assets that have a parent, but since I can't figure that out, I'm going to just limit the path that's returned from an asset.
-        # Our quick fix for now is to add the:
-            # @property
-            # def path(self):
-        # lines to the Asset object. This will only return the first path that's returned from the database instead of the list of them :)
-        
-        filters = [
-            [ 'project', 'is', { 'type': 'Project', 'id': self.PROJECT_ID } ],
-            [ 'sg_status_list', 'is_not', 'oop' ],
-            {
-                'filter_operator': 'any',
-                'filters': [
-                    [ 'sg_path', 'ends_with', f'/{name}' ] for name in names
-                ],
-            },
-            { 
-                'filter_operator': 'all',
-                'filters': [ 
-                    [ 'sg_asset_type', 'is_not', t ] for t in self._untracked_asset_types
-                ], 
-            },
-        ]
 
-        fields = [
-            'code',
-            'sg_path',
-        ]
-
-        assets = self.sg.find('Asset', filters, fields)
+        assets = self.shot_grid_query_helper.get_all_assets_by_name(names)
 
         return set(
             Asset(
@@ -233,3 +179,62 @@ class ShotGridDatabase(Database):
     def delete_asset(self, name):
         asset_id = self.get_asset_id(name)
         self.sg.delete('Asset', asset_id)
+
+
+class ShotGridQueryHelper():
+    _untracked_asset_types = [ 
+                        'Character', 
+                        'FX', 
+                        'Graphic', 
+                        'Matte Painting', 
+                        'Tool', 
+                        'Font'
+                    ]
+    def __init__(self, database: ShotGridDatabase, shot_grid):
+        self.shot_grid_database = database
+        self.shot_grid = shot_grid
+        
+    def _construct_name_filter(self, names: Iterable[str]) -> dict:
+        return {
+            'filter_operator': 'any',
+            'filters': [
+                [ 'sg_path', 'ends_with', name.lower() ] for name in names
+            ],
+        }
+    def _create_base_filter(self) -> list:
+        return [
+            [ 'project', 'is', { 'type': 'Project', 'id': self.shot_grid_database.PROJECT_ID } ],
+            [ 'sg_status_list', 'is_not', 'oop' ],
+            { 
+                'filter_operator': 'all',
+                'filters': [ 
+                    [ 'sg_asset_type', 'is_not', t ] for t in self._untracked_asset_types
+                ], 
+            },
+        ]
+    def _create_base_fields(self) -> list:
+        return [
+            'code',
+            'sg_path',
+        ]
+
+    def _construct_filter_by_path_end_name(self, name: str) -> list:
+        return self._create_base_filter().append(self._construct_name_filter([name]))
+    
+    def _construct_filter_by_path_end_names(self, names: Iterable[str]) -> list:
+        return self._create_base_filter().append(self._construct_name_filter(names))
+    
+    def _get_all_asset_json_by_name(self, filters, fields):
+        assets = self.sg.find('Asset', filters, fields)
+        return assets
+    
+    def get_all_assets_by_name(self, names: Iterable[str], additional_fields: list = []):
+        filters = self._construct_filter_by_path_end_names(names)
+        fields = self._create_base_fields() + additional_fields
+        return self._get_all_asset_json_by_name(filters, fields)
+    
+    def get_one_asset_by_name(self, name: str, additional_fields: list = []):
+        assets = self.get_all_assets_by_name([name], additional_fields)
+        assert len(assets) == 1, f"Expected to find one asset with name {name}, but found {len(assets)}"
+        return assets[0]
+    
