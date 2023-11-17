1b57a7cb320f8707a284262a60a78157d82886e1
Anson Savage <ansonsavage@gmail.com>
2023-11-17 15:11:31 -0700

    By default, bypass the restructure scene graph node and then people can turn it on when they want.

--- pipe/accomplice/software/shared/helper/utilities/houdini_utils.py
+++ pipe/accomplice/software/shared/helper/utilities/houdini_utils.py
@@ -142,6 +142,7 @@ class HoudiniNodeUtils():
             end_null.setDisplayFlag(True)
 
             restructure_scene_graph_node = self.add_restructure_scene_graph_node(end_null)
+            restructure_scene_graph_node.bypass(1)
             self.create_department_usd_rop_node(restructure_scene_graph_node)
 
         def create_import_layout_node(self):
